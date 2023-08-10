c9115d6949676aa8e56f27c51b2f25e29e74ab82
Andres D. Molins <nesitor@gmail.com>
2023-08-11 00:42:18 +0200

    Feature: Implemented publish method.

--- generate-function/main.py
+++ generate-function/main.py
@@ -3,6 +3,7 @@ import logging
 
 from ecies import decrypt, encrypt
 from pydantic import BaseModel
+from typings import Union
 
 logger = logging.getLogger(__name__)
 
@@ -17,7 +18,13 @@ logger.debug("import fastapi")
 from fastapi import FastAPI
 
 logger.debug("local imports")
-from models import APIResponse, generate_request_from_message, VRFResponseHash
+from models import (
+    APIResponse,
+    generate_request_from_message,
+    generate_response_hash_from_message,
+    VRFResponseHash,
+    VRFRandomBytes,
+)
 from utils import generate
 
 logger.debug("imports done")
@@ -36,6 +43,7 @@ async def index():
         "name": "vrf_generate_api",
         "endpoints": [
             "/generate",
+            "/publish"
         ],
     }
 
@@ -60,12 +68,15 @@ async def receive_generate(request: GenerateRequest) -> APIResponse:
         encrypted_hash = encrypt(message.sender, content)
 
         response_hash = VRFResponseHash(
+            num_bytes=generation_request.num_bytes,
+            nonce=generation_request.nonce,
             request_id=generation_request.request_id,
             execution_id=generation_request.execution_id,
+            vrf_request=request.vrf_request,
             random_bytes_hash=encrypted_hash,
         )
 
-        await publish_hash(response_hash, account)
+        await publish_data(response_hash, account)
 
         return APIResponse(
             error=False,
@@ -73,18 +84,49 @@ async def receive_generate(request: GenerateRequest) -> APIResponse:
         )
 
 
-async def publish_hash(response_hash: VRFResponseHash, account: ETHAccount) -> str:
+class PublishRequest(BaseModel):
+    hash_message: str
 
-    channel = f"vrf_{response_hash.requestId.__str__()}"
+
+@app.post("/publish")
+async def receive_publish(request: PublishRequest) -> APIResponse:
+
+    private_key = get_fallback_private_key()
+    account = ETHAccount(private_key=private_key)
+
+    async with AlephClient() as client:
+        message = await client.get_message(item_hash=request.hash_message)
+        response_hash = generate_response_hash_from_message(message)
+
+        decrypted_bytes = await account.decrypt(response_hash.random_bytes_hash)
+        random_bytes = decrypted_bytes[:response_hash.num_bytes]
+
+        response_bytes = VRFRandomBytes(
+            request_id=response_hash.request_id,
+            execution_id=response_hash.execution_id,
+            vrf_request=response_hash.vrf_request,
+            random_bytes=random_bytes,
+        )
+
+        await publish_data(response_bytes, account)
+
+        return APIResponse(
+            error=False,
+            data=response_bytes
+        )
+
+
+async def publish_data(data: Union[VRFResponseHash, VRFRandomBytes], account: ETHAccount):
+
+    channel = f"vrf_{data.requestId.__str__()}"
 
     async with AuthenticatedAlephClient(
             account=account, api_server=API_HOST
     ) as client:
         response, status = await client.create_post(
-            content=response_hash,
+            content=data,
             channel=channel,
         )
 
         # TODO: Check message status
-
-        return response_hash.random_bytes_hash
+        pass
--- models.py
+++ models.py
@@ -12,8 +12,8 @@ class RandomCRN(BaseModel):
 
 class VRFRequest(BaseModel):
     number_bytes: int
-    requestId: UUID
     vrf_function: ItemHash
+    requestId: UUID
     nodes: List[RandomCRN]
 
     def __int__(self):
@@ -23,35 +23,51 @@ class VRFRequest(BaseModel):
 
 class VRFGenerationRequest(BaseModel):
     num_bytes: int
+    nonce: int
     request_id: UUID
     execution_id: UUID
     vrf_function: ItemHash
-    nonce: int
 
     def __int__(self):
         super().__init__(self)
         self.execution_id = uuid4()
 
 
-def generate_request_from_message(message: PostMessage):
+def generate_request_from_message(message: PostMessage) -> VRFGenerationRequest:
     content = message.content
     return VRFGenerationRequest(
         num_bytes=content.num_bytes,
+        nonce=content.nonce,
         request_id=content.request_id,
         vrf_function=ItemHash(content.vrf_function),
-        nonce=content.nonce,
     )
 
 
 class VRFResponseHash(BaseModel):
+    num_bytes: int
+    nonce: int
     requestId: UUID
     execution_id: UUID
+    vrf_request: ItemHash
     random_bytes_hash: str
+
+
+def generate_response_hash_from_message(message: PostMessage) -> VRFResponseHash:
+    content = message.content
+    return VRFResponseHash(
+        num_bytes=content.num_bytes,
+        nonce=content.nonce,
+        request_id=content.request_id,
+        execution_id=content.execution_id,
+        vrf_request=ItemHash(content.vrf_request),
+        random_bytes_hash=content.random_bytes_hash,
+    )
 
 
 class VRFRandomBytes(BaseModel):
     requestId: UUID
     execution_id: UUID
+    vrf_request: ItemHash
     random_bytes: bytes
 
 
