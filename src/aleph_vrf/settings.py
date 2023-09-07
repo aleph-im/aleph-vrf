b00103b0ed3dfb82aec11bcbc26df0ecb9f35c67
Olivier Desenfans <desenfans.olivier@gmail.com>
2023-09-07 11:24:18 +0200

    Feature: user settings (#2)
    Problem: settings are hardcoded.
    
    Solution: use Pydantic settings to allow the user/maintainer to
    customize the install using environment variables or a .env file.

---
+++ src/aleph_vrf/settings.py
@@ -0,0 +1,30 @@
+from pydantic import BaseSettings, Field
+
+
+class Settings(BaseSettings):
+    API_HOST: str = Field(
+        "https://api2.aleph.im",
+        description="URL of the reference aleph.im Core Channel Node.",
+    )
+    CORECHANNEL_AGGREGATE_ADDRESS = Field(
+        "0xa1B3bb7d2332383D96b7796B908fB7f7F3c2Be10",
+        description="Address posting the `corechannel` aggregate.",
+    )
+    CORECHANNEL_AGGREGATE_KEY = Field(
+        "corechannel", description="Key for the `corechannel` aggregate."
+    )
+    FUNCTION: str = Field(
+        "0x111111111111111111111111", description="VRF function to use."
+    )
+    NB_EXECUTORS: int = Field(32, description="Number of executors to use.")
+    NB_BYTES: int = Field(
+        32, description="Number of bytes of the generated random number."
+    )
+
+    class Config:
+        env_prefix = "ALEPH_VRF_"
+        case_sensitive = False
+        env_file = ".env"
+
+
+settings = Settings()
